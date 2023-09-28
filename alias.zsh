cd9e5bd70ff83c6713bb0b603c3c8399a37de29b
Richard Bell <richard.t.bell90@gmail.com>
2023-09-28 09:42:53 +0100

    Update alias.zsh

--- alias.zsh
+++ alias.zsh
@@ -5,6 +5,11 @@ function al () {
   source ~/.zshrc
 }
 
+function setUpstream () {
+  local current_branch=$(git symbolic-ref --short HEAD)
+  git push --set-upstream origin "$current_branch"
+}
+
 alias gs='git status'
 alias cdp='cd ~/projects'
 alias aliases='vim ~/.oh-my-zsh/custom/alias.zsh'
@@ -14,3 +19,4 @@ alias move='git mv'
 alias tidy='git branch --merged | egrep -v "(^\*|master|main|dev)" | xargs git branch -d'
 alias drm='docker rm -f $(docker ps -a -q)'
 alias dps='docker ps --format "table {{.Image}}\t{{.Names}}\t{{.Command}}\t{{.Ports}}" --no-trunc'
+alias scripts="cat package.json | jq '.scripts'"
